a4b77e9d1a7976394caacadbcfce2c38c2fcb9bd
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-07 23:40:12 +0530

    Update 22_datetime_handling.py

--- 01_Python/02_practicals/22_datetime_handling.py
+++ 01_Python/02_practicals/22_datetime_handling.py
@@ -1,1 +1,16 @@
+"""
+Date and time handling in Data Engineering
+"""
+
+from datetime import datetime, timedelta
+
+def main():
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+
+    print("Today:", today.strftime("%Y-%m-%d"))
+    print("Yesterday:", yesterday.strftime("%Y-%m-%d"))
+
+if __name__ == "__main__":
+    main()
 
