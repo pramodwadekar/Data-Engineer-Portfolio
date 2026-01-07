2b58d3defa7de600e02d147c7a30e009efd3808f
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-07 23:45:26 +0530

    Update 29_basic_unit_testing.py

--- 01_Python/02_practicals/29_basic_unit_testing.py
+++ 01_Python/02_practicals/29_basic_unit_testing.py
@@ -1,1 +1,20 @@
+"""
+Basic unit testing for Data Engineering functions
+"""
+
+import unittest
+
+def calculate_total(amounts):
+    return sum(amounts)
+
+class TestETLFunctions(unittest.TestCase):
+
+    def test_calculate_total(self):
+        self.assertEqual(calculate_total([100, 200, 300]), 600)
+
+    def test_empty_list(self):
+        self.assertEqual(calculate_total([]), 0)
+
+if __name__ == "__main__":
+    unittest.main()
 
