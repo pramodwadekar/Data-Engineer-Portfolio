b314fd2c5dc5f034b1068bb87cb1a3e43021d7a4
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-08 00:33:26 +0530

    Create logger.py

---
+++ 01_Python/03_projects/PROJECT 2: API → Database Data Ingestion Pipeline/logger.py
@@ -0,0 +1,12 @@
+"""
+Centralized logging configuration
+"""
+
+import logging
+
+def get_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    return logging.getLogger("API_DB_PIPELINE")
