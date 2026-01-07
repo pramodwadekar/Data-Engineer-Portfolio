1b1feebb7c066187108fb26bc75d757b61bf68e8
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-07 23:34:57 +0530

    Update 12_null_handling.py

--- 01_Python/02_practicals/12_null_handling.py
+++ 01_Python/02_practicals/12_null_handling.py
@@ -1,1 +1,20 @@
+"""
+Handling null values in Data Engineering
+"""
+
+import pandas as pd
+
+def main():
+    df = pd.read_csv("data/sales.csv")
+
+    print("Null count before cleaning:")
+    print(df.isnull().sum())
+
+    df_cleaned = df.dropna()
+
+    print("Null count after cleaning:")
+    print(df_cleaned.isnull().sum())
+
+if __name__ == "__main__":
+    main()
 
