49e2a735a37ffdeac9ddc1f4a22415d7b471b6dd
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-01-07 23:44:53 +0530

    Update 28_performance_optimization.py

--- 01_Python/02_practicals/28_performance_optimization.py
+++ 01_Python/02_practicals/28_performance_optimization.py
@@ -1,1 +1,21 @@
+"""
+Basic performance optimization techniques in Python for DE
+"""
+
+import pandas as pd
+
+def main():
+    file_path = "data/large_sales.csv"
+
+    total_amount = 0
+
+    # Chunk-based processing (memory efficient)
+    for chunk in pd.read_csv(file_path, chunksize=5000):
+        total_amount += chunk["amount"].sum()
+
+    print("Total amount:", total_amount)
+    print("Used chunking to avoid memory issues")
+
+if __name__ == "__main__":
+    main()
 
