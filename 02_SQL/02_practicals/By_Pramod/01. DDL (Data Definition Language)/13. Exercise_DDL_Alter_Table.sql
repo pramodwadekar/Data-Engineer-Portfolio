de5179ecd169ff48b0135df88080e62bd11a8cc6
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-02 01:01:27 +0530

    Create 13. Exercise_DDL_Alter_Table.sql

---
+++ 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/13. Exercise_DDL_Alter_Table.sql
@@ -0,0 +1,19 @@
+-- Exercise DDL Alter Rename
+Alter table Product Alter column ProductId rename to PId;
+
+
+-- Exercise DDL Alter Modify DataType
+alter table Product alter column price Integer;
+
+
+-- Exercise DDL Alter Add Column
+alter table Product add Color varchar(10);
+
+-- Exercise DDL Alter Add Column Constraint
+alter table Product add Review varchar(10) check (Review in ('Bad', 'Good', 'Excellent'));
+
+-- Exercise DDL Alter DropTable Vendoroffering 
+DROP TABLE IF EXISTS VendorOffering;
+
+-- Exercise DDL DropTable Product 
+drop table Product CASCAD
