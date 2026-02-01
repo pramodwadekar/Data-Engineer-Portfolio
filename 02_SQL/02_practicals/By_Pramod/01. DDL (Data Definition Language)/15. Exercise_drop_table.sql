68e143178c6ecf393729db8e2f48934022bea90f
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-02 01:11:40 +0530

    Create 15. Exercise_drop_table.sql

---
+++ 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/15. Exercise_drop_table.sql
@@ -0,0 +1,8 @@
+1: Exercise DDL Alter DropTable Vendoroffering 
+DROP TABLE IF EXISTS VendorOffering;
+
+1: Exercise DDL DropTable Product 
+drop table Product CASCAD
+
+Exercise DDL Alter DropColumn 
+alter table Product drop column column Price;
