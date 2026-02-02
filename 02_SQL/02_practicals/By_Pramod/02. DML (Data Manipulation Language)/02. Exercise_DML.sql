1e22695653c859b3c12b9d2104d1cec1b8520af1
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-02 22:53:14 +0530

    Create 02. Exercise_DML.sql

---
+++ 02_SQL/02_practicals/By_Pramod/02. DML (Data Manipulation Language)/02. Exercise_DML.sql
@@ -0,0 +1,9 @@
+--  Exercise DML InsertCustomer:
+insert into Customer (CUSTID, CUSTNAME, GENDER) values ('C118', 'Denver Null)
+
+--  Exercise DML Insert Vendor:
+insert into Vendor values ('V113', 'Alpha Traders', 7766671890, 4), ('V114', 'Max Retailers', NULL, 2), 
+('V115', 'Turning Tech, 6578567890, 3), ("V116', 'Dexter and Co', NULL, 4)
+
+--  Exercise DMI InsertProduct:
+insert into Product (PRODUCTID, PNAME, BRAND, REVIEW) VALUES ('P111', 'Jacket', 'Woodland', NULL)
