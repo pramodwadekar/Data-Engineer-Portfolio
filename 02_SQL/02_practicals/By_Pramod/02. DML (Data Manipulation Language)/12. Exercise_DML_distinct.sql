646c526c195d17814bf9f4ab134b365d5fab1261
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-03 00:15:31 +0530

    Create 12. Exercise_DML_distinct.sql

---
+++ 02_SQL/02_practicals/By_Pramod/02. DML (Data Manipulation Language)/12. Exercise_DML_distinct.sql
@@ -0,0 +1,13 @@
+-- 🔹 Problem Statement
+-- Write a query to display the categories of all the products.
+-- Remove the duplicate records from the output if any.
+
+SELECT DISTINCT PCategory
+FROM Product;
+
+-- 🎯 Expected Output (from your sample Product table)
+-- PCATEGORY
+-- NULL
+-- Accessories
+-- Clothing
+-- Shoes
