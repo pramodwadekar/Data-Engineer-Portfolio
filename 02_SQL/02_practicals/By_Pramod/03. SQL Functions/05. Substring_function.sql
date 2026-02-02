8d49806a80e510327e0b45d5d1c10a42e95ae706
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-03 02:27:41 +0530

    Create 05. Substring_function.sql

---
+++ 02_SQL/02_practicals/By_Pramod/03. SQL Functions/05. Substring_function.sql
@@ -0,0 +1,42 @@
+-- 🔎 Substring Function Syntax
+-- SUBSTR(value, start_position, length)
+
+-- 1. Extract first four characters of city names
+SELECT City, SUBSTR(City, 1, 4) AS FIRST4
+FROM Weather;
+
+
+-- 2. Extract 10 characters starting from second position
+SELECT City, SUBSTR(City, 2, 10) AS TEN_FROM_2
+FROM Weather;
+
+
+-- 3. Extract whole string starting from third character
+SELECT City, SUBSTR(City, 3) AS ALL_FROM_3
+FROM Weather;
+
+
+-- 4. Extract 2 characters starting from seventh position
+SELECT City, SUBSTR(City, 7, 2) AS TWO_FROM_7
+FROM Weather;
+
+
+
+-- 📘 Tryout_CharacterFunctionsPart2 Exercises
+-- 1. Display city and first four characters
+SELECT City, SUBSTR(City, 1, 4) AS FIRST4
+FROM Weather;
+
+
+-- 2. Display 4 characters starting from second character
+-- (Example: 'ondo', 'ysor')
+SELECT City, SUBSTR(City, 2, 4) AS FOUR_FROM_2
+FROM Weather;
+
+
+-- 3. Display names of cities starting from third character
+-- (Example: 'ndon', 'ngalore')
+SELECT City, SUBSTR(City, 3) AS ALL_FROM_3
+FROM Weather;
+
+
