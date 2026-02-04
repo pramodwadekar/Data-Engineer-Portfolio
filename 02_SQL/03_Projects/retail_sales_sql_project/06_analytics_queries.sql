852c25376aaf7788e0f8496e099046fd787ac9e6
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-04 23:05:59 +0530

    Create 06_analytics_queries.sql

---
+++ 02_SQL/03_Projects/retail_sales_sql_project/06_analytics_queries.sql
@@ -0,0 +1,20 @@
+-- Total Revenue
+SELECT SUM(sales_amount) AS total_revenue
+FROM fact_sales;
+
+-- Monthly Revenue
+SELECT
+    DATE_TRUNC('month', order_date) AS month,
+    SUM(sales_amount) AS revenue
+FROM fact_sales
+GROUP BY month
+ORDER BY month;
+
+-- Top 5 Customers
+SELECT
+    customer_id,
+    SUM(sales_amount) AS total_spent
+FROM fact_sales
+GROUP BY customer_id
+ORDER BY total_spent DESC
+LIMIT 5;
