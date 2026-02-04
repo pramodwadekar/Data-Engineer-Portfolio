3638ccf13839f33c389283866f23bde15622ddea
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-02-04 23:00:42 +0530

    Delete 02_SQL/03_projects/01. Online Shop Management System directory

--- 02_SQL/03_projects/01. Online Shop Management System/09_views.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW OrderSummary AS
-SELECT o.OrderID, c.CustomerName, o.OrderDate, pay.Amount
-FROM Orders o
-JOIN Customers c ON o.CustomerID = c.CustomerID
-JOIN Payments pay ON o.OrderID = pay.OrderID;
-
--- Use the view
-SELECT * FROM OrderSummary;
