920cf142639e5bfeaee2ee6cdf9e880313dc8c8f
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-06-26 19:03:20 +0530

    Update 05.Create_composite_primary_key.sql

--- 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/05.Create_composite_primary_key.sql
+++ 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/05.Create_composite_primary_key.sql
@@ -4,3 +4,14 @@ CREATE TABLE Result (
   Marks NUMERIC(3),
   CONSTRAINT result_pk PRIMARY KEY (StudentId, CourseId)
 );
+
+OR 
+
+CREATE TABLE Student (
+    Student_ID INT,
+    Course_ID INT,
+    Student_Name VARCHAR(100),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    PRIMARY KEY (Student_ID, Course_ID)
+);
