dc78ff0e92536c6126507113c9de0c4aa30cf8fc
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-06-26 19:24:21 +0530

    Implement check constraints for Student table
    Added check constraints to enforce data integrity in Student table.

--- 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/06. Check_constraints.sql
+++ 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/06. Check_constraints.sql
@@ -1,5 +1,27 @@
+/*Check: Helps to achive domain integrity by limiting the acceptable data formats and ranges.
+      Check constraint act like a rule to restrict invalid information. */
+/* IN Format */
 CREATE TABLE Student (
   StudentId INTEGER,
   FName VARCHAR(10),
   Gender CHAR(1) CHECK (Gender IN ('M', 'F'))
 );
+
+/* Like, Start, End, Anywhere, secondword, secondlast Format */
+CREATE TABLE STUDENT (
+    STUDENTID INTEGER,
+    FNAME VARCHAR(10) CHECK (FNAME LIKE 'A%'),      /* Starts with A */
+    LNAME VARCHAR(10) CHECK (LNAME LIKE '%E'),      /* Ends with E */
+    SNAME VARCHAR(10) CHECK (SNAME LIKE '%E%'),     /* Contains E anywhere */
+    CITY VARCHAR(10) CHECK (CITY LIKE '_A%'),       /* Second character is A */
+    STATE VARCHAR(10) CHECK (STATE LIKE '%A_'),     /* Second last character is A */
+    COUNTRY VARCHAR(10) CHECK (COUNTRY LIKE 'A_E%') /* Starts with A, second character any, third character E */
+);
+
+/* Range Between Format*/
+CREATE TABLE STUDENT(
+	STUDENTID INTEGER, 
+	FNAME VARCHAR(10) CHECK (FNAME LIKE 'A%')
+	FEES VARCHAR(10) CHECK (FEES BETWEEN 2500 AND 60000)
+);
+
