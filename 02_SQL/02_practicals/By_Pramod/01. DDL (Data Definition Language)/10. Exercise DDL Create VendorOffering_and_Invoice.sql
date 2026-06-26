0add6c6ef0e37f06d7c22183a10367d618d174f2
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-06-26 19:46:33 +0530

    Create VendorOffering and Invoice tables
    Created tables for VendorOffering and Invoice with specified constraints and relationships.

--- 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/10. Exercise DDL Create VendorOffering_and_Invoice.sql
+++ 02_SQL/02_practicals/By_Pramod/01. DDL (Data Definition Language)/10. Exercise DDL Create VendorOffering_and_Invoice.sql
@@ -1,8 +1,29 @@
 -- Exercise DDL Create VendorOffering
+-- Composite Primary key example:
+/* Create the VendorOffering table as per the structure given below: 
+| Column Name | Data Type and Size | Constraint                         | Description                                                      |
+| ----------- | ------------------ | ---------------------------------- | ---------------------------------------------------------------- |
+| VendorId    | VARCHAR(5)         | FOREIGN KEY, PRIMARY KEY           | Existing `VendorId` present in the **Vendor** table              |
+| ProductId   | VARCHAR(5)         | FOREIGN KEY, NOT NULL, PRIMARY KEY | Existing `ProductId` present in the **Product** table            |
+| Price       | NUMERIC(7,2)       | NOT NULL, CHECK                    | Price offered by the vendor in INR, should be greater than **0** |
+*/
 CREATE TABLE Vendoroffering (VendorId VARCHAR(5) REFERENCES Vendor (VendorId),
 ProductId VARCHAR(5) NOT NULL REFERENCES Product (ProductId),
 Price NUMERIC(7,2) NOT NULL CHECK (Price>0),
 PRIMARY KEY (VendorId, ProductId));
+
+-- Composite foregn key in same table in vendoroffering 
+/*Create the invoice table as per the structure given below:
+| Column Name       | Data Type and Size | Constraint         | Description                                                          |
+| ----------------- | ------------------ | ------------------ | -------------------------------------------------------------------- |
+| InvoiceId         | VARCHAR(5)         | PRIMARY KEY, CHECK | Unique `InvoiceId` for every invoice, should start with **'IN'**     |
+| CustId            | VARCHAR(5)         | FOREIGN KEY        | Existing `CustId` present in **Customer** table                      |
+| VendorId          | VARCHAR(5)         | FOREIGN KEY        | Existing `{VendorId, ProductId}` present in **VendorOffering** table |
+| ProductId         | VARCHAR(5)         | FOREIGN KEY        | Existing `{VendorId, ProductId}` present in **VendorOffering** table |
+| QuantityPurchased | NUMERIC(2)         | NOT NULL           | Quantity (number of products) purchased by the customer              |
+| Discount          | NUMERIC(3,1)       | NOT NULL           | Discount offered on the purchase (in percentage)                     |
+| PurchaseDate      | DATE               | NOT NULL           | Date of purchase                                                     |
+*/
 
 -- Exercise DDL Create Invoice
 CREATE TABLE Invoice (InvoiceId VARCHAR(5) PRIMARY KEY CHECK(InvoiceId LIKE 'IN%'),
