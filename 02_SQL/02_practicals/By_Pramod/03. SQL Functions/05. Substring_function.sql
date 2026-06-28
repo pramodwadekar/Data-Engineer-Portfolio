cb75d6934749db10a2c596b1a129cbf9f71ba907
Pramod Subhash Wadekar <126280227+pramodwadekar@users.noreply.github.com>
2026-06-28 17:35:35 +0530

    Add SQL examples for substring function usage
    Added SQL queries to demonstrate substring extraction from city names.

--- 02_SQL/02_practicals/By_Pramod/03. SQL Functions/05. Substring_function.sql
+++ 02_SQL/02_practicals/By_Pramod/03. SQL Functions/05. Substring_function.sql
@@ -4,21 +4,53 @@
 -- 1. Extract first four characters of city names
 SELECT City, SUBSTR(City, 1, 4) AS FIRST4
 FROM Weather;
+/*
+City        FIRST4
+----------  ------
+London      Lond
+Mysore      Myso
+Bangalore   Bang
+*/
 
 
 -- 2. Extract 10 characters starting from second position
 SELECT City, SUBSTR(City, 2, 10) AS TEN_FROM_2
 FROM Weather;
+/*
+City        TEN_FROM_2
+----------  ----------
+London      ondon
+Mysore      ysore
+Bangalore   angalore
+Mumbai      umbai
+*/
 
 
 -- 3. Extract whole string starting from third character
 SELECT City, SUBSTR(City, 3) AS ALL_FROM_3
 FROM Weather;
-
+/*
+City        ALL_FROM_3
+----------  ----------
+London      ndon
+Mysore      sore
+Bangalore   ngalore
+Mumbai      mbai
+Delhi       lhi
+*/
 
 -- 4. Extract 2 characters starting from seventh position
 SELECT City, SUBSTR(City, 7, 2) AS TWO_FROM_7
 FROM Weather;
+/*
+City        TWO_FROM_7
+----------  ----------
+London
+Mysore
+Bangalore   or
+Mumbai
+Delhi
+*/
 
 
 
